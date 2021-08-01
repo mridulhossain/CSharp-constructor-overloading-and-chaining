f648a1a5dee9d82d110a1dc760372c2ab3f922b4
mridulhossain <mridulfeni94@gmail.com>
2021-08-01 18:04:49 +0600

    constructor,constructor overloading and constructor chaining

---
+++ DataTypeIO/Car.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DataTypeIO
+{
+    public class Car
+    {
+        public int Fuel;
+        public string Key;
+        public string Driver;
+
+        //-------constructor chaining--------
+        public Car() : this(1, "tanjib")
+        {
+
+        }
+
+        //-------end--------
+
+        // when constructor calling a constructor that is constructor chaining
+        // and in a class constructor More than one it's called constructor over loading
+
+        public Car(int fuel, string key)
+        {
+            this.Fuel = fuel;
+            this.Key = key;
+            Console.WriteLine(this.Fuel + "," + this.Key);
+        }
+
+
+        public Car(int fuel, string key, string driver)
+        {
+            this.Fuel = fuel;
+            this.Key = key;
+            this.Driver = driver;
+            Console.WriteLine(this.Fuel + "," + this.Key+","+this.Driver);
+        }
+
+
+
+
+    }
+}
---
+++ DataTypeIO/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace DataTypeIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var toyotaNoha = new Car(); // constructor chaining
+            var toyotaPremio = new Car(10,"mridul");
+            var toyotaPremioWithDriver = new Car(10, "mridul","driver");
+        } 
+    }
+}
